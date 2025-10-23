192d74059b58fa718c80a1dea4b821fbcfc7c85c
noneals <124405266+noneals@users.noreply.github.com>
2025-10-24 00:37:12 +0300

    Update sort.h just fix Betty style issues

--- sort.h
+++ sort.h
@@ -1,16 +1,12 @@
-#ifndef _SORT_H_
-#define _SORT_H_
+#ifndef SORT_H
+#define SORT_H
 
-/*_______________________Standards Libraries_______________________*/
-
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/*_______________________Structures Prototypes_______________________*/
-
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -22,14 +18,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/*_______________________Typedef Struct Prototypes_______________________*/
-
-/*_______________________Functions  Prototypes_______________________*/
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void bubble_sort(int *array, size_t size);
 
-
-#endif /* _SORT_H_ */
+#endif /* SORT_H */
