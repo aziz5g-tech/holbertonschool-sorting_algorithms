2fd481bb4d85b2e3fe17422736bd631b5dd308db
aziz5g-tech <bogico.sa@gmail.com>
2025-10-24 04:32:43 +0300

    Implement Lomuto partition function

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,10 +10,39 @@
  */
 void swap(int *array, size_t i, size_t j)
 {
-	/* Store the value at index i in a temporary variable */
 	int temp = array[i];
-	/* Assign the value at index j to index i */
 	array[i] = array[j];
-	/* Assign the value stored in the temporary variable to index j */
 	array[j] = temp;
+}
+
+/**
+ * lomuto_partition - Partitions a portion of an array using the Lomuto
+ *                    partition scheme.
+ * @array: Pointer to the array of integers to be partitioned.
+ * @low: The low index of the portion to be partitioned.
+ * @high: The high index of the portion to be partitioned.
+ * @size: The size of the array.
+ *
+ * Return: The index of the pivot in the partitioned array.
+ */
+
+static int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high], i = (low - 1), j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			swap(array, i, j);
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	swap(array, i + 1, high);
+	if (high != (i + 1))
+		print_array(array, size);
+
+	return (i + 1);
 }
